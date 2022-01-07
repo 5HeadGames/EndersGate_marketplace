7e80eaa688f31ae463623677f53c71174104a158
Rcontre360 <rcontreraspimentel@gmail.com>
2022-01-07 17:26:15 -0400

    Finished login connection with redux

--- pages/profile2.tsx
+++
@@ -1,9 +0,0 @@
-import ProfileComponent from "@shared/components/Profile/profile";
-import React from "react";
-import "shared/firebase";
-
-const Profile = () => {
-  return <ProfileComponent />;
-};
-
-export default Profile;
--- redux/actions/users.ts
+++ redux/actions/users.ts
@@ -1,5 +1,9 @@
 import {createAction, createAsyncThunk} from "@reduxjs/toolkit";
-import {getAuth, signInWithEmailAndPassword} from "firebase/auth";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 import {readUser, writeUser} from "shared/firebase";
 import * as actionTypes from "../constants";
 
@@ -15,32 +19,47 @@ const auth = getAuth();
 
 export const onLoginUser = createAsyncThunk(
   actionTypes.LOGIN_USER,
-  async function prepare(userData: {address: string} | {email: string; password: string}) {
-    console.log(userData)
-    return;
-    if ((userData as any).address) {
+  async function prepare(userData: {email?: string; password?: string; address: string}) {
+    const placeholderData = {
+      email: "",
+      name: "",
+      profile_picture: "",
+      userStatus: "",
+      address: (userData as any).address,
+    };
+    if (userData.email) {
+      try {
+        const {user: userAuth} = await signInWithEmailAndPassword(
+          auth,
+          (userData as any).email,
+          (userData as any).password
+        );
+        const user = await readUser(`users/${userAuth.uid}`);
+        return user;
+      } catch (err) {
+        const {user: userAuth} = await createUserWithEmailAndPassword(
+          auth,
+          userData.email,
+          userData.password
+        );
+        const user = await readUser(`users/${userData.address}`);
+        writeUser(
+          (userData as any).address,
+          {email: userData.email},
+        );
+        writeUser(
+          userAuth.uid,
+          {email: userData.email}
+        );
+
+        return user || placeholderData;
+      }
+    } else {
       const user = await readUser(`users/${(userData as any).address}`);
-      const placeholderData = {
-        email: "",
-        name: "",
-        profile_picture: "",
-        userStatus: "",
-        address: (userData as any).address,
-      };
       if (!user) {
         writeUser((userData as any).address, placeholderData);
       }
       return user || placeholderData;
-    } else {
-      const {user: userAuth} = await signInWithEmailAndPassword(
-        auth,
-        (userData as any).email,
-        (userData as any).password
-      );
-      const user = await readUser(`users/${userAuth.uid}`)
-      return user
     }
   }
 );
-
-
