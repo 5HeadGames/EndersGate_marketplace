3552e83d928d42594325715e648ae345b4fda70b
@acontrerasShokWorks <acontreras@shokworks.io>
2022-01-10 20:44:40 -0400

    Added copy to clipboard and external link to wallet

--- shared/components/Profile/index/index.tsx
+++ shared/components/Profile/index/index.tsx
@@ -4,6 +4,9 @@ import { useAppSelector } from "redux/store";
 import { useRouter } from "next/router";
 import clsx from "clsx";
 import { Icons } from "@shared/const/Icons";
+import { CopyOutlined, SelectOutlined } from "@ant-design/icons";
+import { AddressText } from "@shared/components/common/specialFields/SpecialFields";
+import { useToasts } from "react-toast-notifications";
 
 const ProfileIndexPage = () => {
   const user = useAppSelector((state) => state.user);
@@ -14,11 +17,14 @@ const ProfileIndexPage = () => {
     // }
     console.log(user);
   }, [user]);
+
+  const { addToast } = useToasts();
+
   return (
     <>
       <div
         className={clsx(
-          "flex flex-col border border-overlay-border p-4 rounded-md h-40 relative overflow-hidden mb-10"
+          "flex flex-col justify-between border border-overlay-border p-4 rounded-t-md h-52 relative overflow-hidden"
         )}
       >
         <img
@@ -37,10 +43,36 @@ const ProfileIndexPage = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col w-full">
+      <div className="flex justify-between border border-t-0 border-overlay-border p-4 rounded-b-md">
+        <Typography type="subTitle" className="text-primary">
+          Harmony Address: <AddressText text={user.address} />
+        </Typography>
+        <div className="flex items-center text-primary gap-4">
+          <div
+            onClick={() => {
+              navigator.clipboard.writeText(user.address);
+              addToast("Copied to clipboard", { appearance: "info" });
+            }}
+            className="cursor-pointer"
+          >
+            <CopyOutlined />
+          </div>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={`https://explorer.harmony.one/address/${user.address}`}
+          >
+            <SelectOutlined />
+          </a>
+        </div>
+      </div>
+      <div className="flex flex-col w-full mt-10">
         <div className="flex justify-between w-full">
           <Typography type="title" className="text-white">
             Activities
+          </Typography>
+          <Typography type="span" className="text-primary">
+            View More
           </Typography>
         </div>
         <hr className="w-full mt-4" />
