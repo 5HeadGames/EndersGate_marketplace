1ee5f6735170fccbc5a72d2d519486a5971c04a5
@acontrerasShokWorks <acontreras@shokworks.io>
2022-01-10 22:22:03 -0400

    Fixed marketplace filter

--- shared/components/Marketplace/marketplace.tsx
+++ shared/components/Marketplace/marketplace.tsx
@@ -9,16 +9,18 @@ import NftCard from 'shared/components/Marketplace/itemCard'
 import FiltersBoard from "./filters/filters";
 import Styles from "./styles.module.scss";
 import clsx from "clsx";
+import { DropdownMenu } from "../common/dropdownMenu";
+import { DropdownActions } from "../common/dropdownActions/dropdownActions";
 
 const MarketplaceComponent = () => {
   const [currentOrder, setCurrentOrder] = React.useState("lowest_price");
 
   const orderMapper = {
-    lowest_price: 'Lowest Price',
-    highest_price: 'Highest Price',
-    recently_listed: 'Recently Listed',
-    older_listed: 'Older Listed',
-  }
+    lowest_price: "Lowest Price",
+    highest_price: "Highest Price",
+    recently_listed: "Recently Listed",
+    older_listed: "Older Listed",
+  };
 
   const [filter, setFilter] = React.useState({
     avatar: false,
@@ -50,34 +52,33 @@ const MarketplaceComponent = () => {
     <div className="w-full flex xl:flex-row flex-col md:px-8 px-4 pt-36 pb-10">
       <FiltersBoard filter={filter} setFilter={setFilter} />
       <div className="xl:w-2/3 xl:mt-0 mt-6 flex flex-col">
-        <div >
+        <div>
           <div className="w-full flex justify-between sm:flex-row flex-col">
             <h3 className="text-2xl text-primary ml-4">619,801 Axies</h3>
             <div className="flex">
-              <CollapseMenu
+              <DropdownActions
                 title={orderMapper[currentOrder]}
-                classes={{
-                  container: "border border-primary border-2 p-2 rounded-md",
-                  root: "mr-4",
-                }}
-              >
-                <div className="bg-secondary flex flex-col justify-center items-center z-100 cursor-pointer">
-                  {[
-                    "lowest_price",
-                    "highest_price",
-                    "recently_listed",
-                    "older_listed",
-                  ].map((title) => (
-                    <div
-                      className="p-2 text-primary hover:bg-primary hover:text-secondary w-full text-center"
-                      onClick={() => setCurrentOrder(title)}
-                    >
-                      {orderMapper[title]}
-                    </div>
-                  ))}
-                </div>
-              </CollapseMenu>
-              <div className=" border-2 border rounded-md overflow-hidden border-primary flex justify-center items-center text-primary h-10">
+                actions={[
+                  {
+                    label: "Lowest Price",
+                    onClick: () => setCurrentOrder("lowest_price"),
+                  },
+                  {
+                    label: "Highest Price",
+                    onClick: () => setCurrentOrder("highest_price"),
+                  },
+                  {
+                    label: "Recently Listed",
+                    onClick: () => setCurrentOrder("recently_listed"),
+                  },
+                  {
+                    label: "Older Listed",
+                    onClick: () => setCurrentOrder("older_listed"),
+                  },
+                ]}
+              />
+
+              <div className=" border-2 border rounded-md overflow-hidden border-primary flex justify-center items-center text-primary h-10 ml-4">
                 <div className="flex flex-1 justify-center items-center text-primary h-10 border-r-2 border-primary p-2 cursor-pointer hover:bg-primary hover:text-secondary">
                   <AppstoreOutlined />
                 </div>
@@ -87,12 +88,10 @@ const MarketplaceComponent = () => {
               </div>
             </div>
           </div>
-          <div className='flex flex-wrap w-full justify-center items-center'>
-            {
-              new Array(6).fill(0).map(() => (
-                <NftCard classes={{root: 'm-4'}} />
-              ))
-            }
+          <div className="flex flex-wrap w-full justify-center items-center">
+            {new Array(6).fill(0).map(() => (
+              <NftCard classes={{ root: "m-4" }} />
+            ))}
           </div>
         </div>
       </div>
---
+++ shared/components/common/dropdownActions/dropdownActions.tsx
@@ -0,0 +1,71 @@
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+import { DotsHorizontalIcon } from "@heroicons/react/solid";
+import clsx from "clsx";
+import { Typography } from "../typography";
+import { Icons } from "@shared/const/Icons";
+
+export const DropdownActions: React.FC<any> = ({ title, actions }) => {
+  return (
+    <div>
+      <Menu as="div" className="relative inline-block text-left">
+        {({ open }) => {
+          console.log("open", open);
+          return (
+            <>
+              <div>
+                <Menu.Button className="inline-flex justify-center w-full font-medium bg-transparent focus:outline-none">
+                  <div className="flex justify-center items-center cursor-pointer rounded-md border-2 border-primary p-2">
+                    <img
+                      src={Icons.upArrow}
+                      style={{ transform: `rotate(${open ? "180deg" : "0"})` }}
+                    />
+                    <Typography type="subTitle" className="text-primary ml-2">
+                      {title}
+                    </Typography>
+                  </div>
+                </Menu.Button>
+              </div>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute top-0 right-3 z-20 md:mt-7 origin-top-right bg-overlay divide-y shadow-lg rounded-[6px] focus:outline-none">
+                  <div className="py-2">
+                    {actions.map((item: any, i: number) => {
+                      // const active = router.asPath === subItem.href;
+                      return (
+                        <Menu.Item key={`item-${i}`}>
+                          {() => (
+                            <div
+                              className={clsx(
+                                "flex justify-start items-center py-[10px] px-3 text-primary hover:bg-primary hover:text-white",
+                                "whitespace-nowrap"
+                              )}
+                              onClick={() => {
+                                item.onClick();
+                              }}
+                            >
+                              <button className={clsx(item.className)}>
+                                {item.label}
+                              </button>
+                            </div>
+                          )}
+                        </Menu.Item>
+                      );
+                    })}
+                  </div>
+                </Menu.Items>
+              </Transition>
+            </>
+          );
+        }}
+      </Menu>
+    </div>
+  );
+};
