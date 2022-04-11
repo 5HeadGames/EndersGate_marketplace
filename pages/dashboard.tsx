de66696fc05bb12de375b1ea015514a6df4ab872
Rcontre360 <rcontreraspimentel@gmail.com>
2022-04-11 17:55:22 -0400

    finished moralis test

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,6 @@
 import DashboardComponent from "@shared/components/Dashboard/dashboard";
 import React from "react";
+import Moralis from "moralis";
 
 const Dashboard = () => {
   return <DashboardComponent />;
