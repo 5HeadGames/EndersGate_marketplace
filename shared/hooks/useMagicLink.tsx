35fd8b6a0acc358bac80639be472b526c3f32f7c
Andres Contreras <andrescontrerasoviedo740@gmail.com>
2022-10-31 21:38:09 -0400

    added magic link hook

---
+++ shared/hooks/useMagicLink.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { Magic } from "magic-sdk";
+import { ConnectExtension } from "@magic-ext/connect";
+import Web3 from "web3";
+
+export default function useMagicLink() {
+  const [account, setAccount] = useState<any>(null);
+  const [loading, setLoading] = useState<any>(null);
+  const [web3, setWeb3] = useState<any>(null);
+  const [magic, setMagic] = useState<any>(null);
+
+  if (
+    typeof window !== "undefined" &&
+    magic == null &&
+    process.env.NEXT_PUBLIC_MAGIC_KEY
+  ) {
+    // Client-side-only code
+    const key = process.env.NEXT_PUBLIC_MAGIC_KEY
+      ? process.env.NEXT_PUBLIC_MAGIC_KEY
+      : "";
+    const chainId = process.env.NEXT_PUBLIC_POLYGON_ID
+      ? process.env.NEXT_PUBLIC_POLYGON_ID
+      : "";
+    const rpcUrl = process.env.NEXT_PUBLIC_SECONDARY_RPC
+      ? process.env.NEXT_PUBLIC_SECONDARY_RPC
+      : "";
+    const magic: any = new Magic(key, {
+      network: {
+        rpcUrl: rpcUrl,
+        chainId: parseInt(chainId),
+      },
+      locale: "en_US",
+      extensions: [new ConnectExtension()],
+    });
+    setMagic(magic);
+    setWeb3(new Web3(magic.rpcProvider));
+  }
+
+  const sendTransaction = async () => {
+    const publicAddress = (await web3.eth.getAccounts())[0];
+    const txnParams = {
+      from: publicAddress,
+      to: publicAddress,
+      value: web3.utils.toWei("0.01", "ether"),
+      gasPrice: web3.utils.toWei("30", "gwei"),
+    };
+    web3.eth
+      .sendTransaction(txnParams as any)
+      .on("transactionHash", (hash: any) => {
+        console.log("the txn hash that was returned to the sdk:", hash);
+      })
+      .then((receipt: any) => {
+        console.log("the txn receipt that was returned to the sdk:", receipt);
+      })
+      .catch((error: any) => {
+        console.log(error);
+      });
+  };
+
+  const login = async (dispatch: any) => {
+    console.log("login");
+    setLoading(true);
+    try {
+      const publicAddress = (await web3.eth.getAccounts())[0];
+      setAccount(publicAddress);
+    } catch (error) {
+      console.log(error, "aqui");
+    }
+    setLoading(false);
+  };
+
+  return {
+    loading,
+    login,
+    sendTransaction,
+    account,
+  };
+}
