13cfd3a24f6ddb0295130329ac0f296276a464be
Andres Contreras <andrescontrerasoviedo740@gmail.com>
2022-11-12 08:37:12 -0400

    fixed errors

--- shared/components/connectors/url.ts
+++ shared/components/connectors/url.ts
@@ -1,7 +1,7 @@
 import { initializeConnector } from "@web3-react/core";
-import { Url } from "@web3-react/url";
+// import { Url } from "@web3-react/url";
 import { URLS } from "../chains";
 
-export const [url, hooks] = initializeConnector<Url>(
-  (actions) => new Url({ actions, url: URLS[1][0] }),
-);
+export const [url, hooks] = initializeConnector<any>((actions) => {
+  return { actions, url: URLS[1][0] };
+});
