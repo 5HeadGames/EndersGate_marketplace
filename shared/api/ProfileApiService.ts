b56cb590d92a40b2c9b3595d437337ad1f27a48f
Andres Contreras <andrescontrerasoviedo740@gmail.com>
2023-04-08 19:51:10 -0400

    adding

--- shared/api/ProfileApiService.ts
+++ shared/api/ProfileApiService.ts
@@ -1,17 +1,17 @@
-import { AxiosInstance } from 'axios';
-import axiosClient from './AxiosClientConfig';
-import { UserType } from 'interfaces/index';
-import { UpdatePasswordRequest } from './dto/Profile.dto';
+import { AxiosInstance } from "axios";
+import axiosClient from "./AxiosClientConfig";
+// import { UserType } from 'interfaces/index';
+import { UpdatePasswordRequest } from "./dto/Profile.dto";
 
 class Service {
-	constructor(private client: AxiosInstance) {}
-	async getUser(): Promise<UserType> {
-		return this.client.get('/users');
-	}
+  constructor(private client: AxiosInstance) {}
+  async getUser(): Promise<any> {
+    return this.client.get("/users");
+  }
 
-	async updatePassword(data: UpdatePasswordRequest): Promise<any> {
-		return this.client.post('/profile/update-password', data);
-	}
+  async updatePassword(data: UpdatePasswordRequest): Promise<any> {
+    return this.client.post("/profile/update-password", data);
+  }
 }
 
 export const ProfilApiService = new Service(axiosClient);
