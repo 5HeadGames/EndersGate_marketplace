44ec3e52451b438d68286d123f65bfd843201380
AN-Drew207 <andrescontrerasoviedo740@gmail.com>
2023-07-24 19:48:43 -0400

    cart comic

--- shared/components/Comics/Modals/ModalCartComics.tsx
+++ shared/components/Comics/Modals/ModalCartComics.tsx
@@ -8,12 +8,6 @@ import { Dialog, Transition } from "@headlessui/react";
 import { findSum } from "@shared/components/common/specialFields/SpecialFields";
 
 export const useCartComicsModal = () => {
-  const { providerName } = useSelector((state: any) => state.layout.user);
-
-  const { showWallet } = useMagicLink();
-
-  const dispatch = useDispatch();
-
   const [isShow, setIsShow] = useState(false);
   const cancelButtonRef = useRef<HTMLDivElement>(null);
 
@@ -25,259 +19,58 @@ export const useCartComicsModal = () => {
     setIsShow(true);
   };
 
-  const Modal = useCallback(
-    ({
-      cart,
-      removeAll,
-      messageBuy,
-      // itemsCart,
-      priceMatic,
-      isMatic,
-      tokensAllowed,
-      tokenSelected,
-      setTokenSelected,
-      isShow,
-      buy,
-      noClose,
-    }) => {
-      return (
-        <Transition.Root show={isShow} as={Fragment}>
-          <Dialog
-            as="div"
-            static
-            className="fixed inset-0 overflow-y-auto"
-            style={{
-              zIndex: 15000,
-            }}
-            initialFocus={cancelButtonRef}
-            open={isShow}
-            onClose={noClose ? () => {} : hide}
-          >
-            <div className="flex items-center justify-center pb-20 pt-4 min-h-screen text-center sm:block sm:p-0 bg-[#000000bb]">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-              >
-                <Dialog.Overlay className="fixed inset-0 bg-transparent-45 transition-opacity" />
-              </Transition.Child>
+  const Modal = useCallback(({ isShow, children, noClose }) => {
+    return (
+      <Transition.Root show={isShow} as={Fragment}>
+        <Dialog
+          as="div"
+          static
+          className="fixed inset-0 overflow-y-auto"
+          style={{
+            zIndex: 15000,
+          }}
+          initialFocus={cancelButtonRef}
+          open={isShow}
+          onClose={noClose ? () => {} : hide}
+        >
+          <div className="flex items-center justify-center pb-20 pt-4 min-h-screen text-center sm:block sm:p-0 bg-[#000000bb]">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-transparent-45 transition-opacity" />
+            </Transition.Child>
 
-              {/* This element is to trick the browser into centering the modal contents. */}
-              <span
-                className="hidden sm:inline-block sm:align-middle sm:h-screen"
-                aria-hidden="true"
-              >
-                &#8203;
-              </span>
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                enterTo="opacity-100 translate-y-0 sm:scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              >
-                <div className="inline-block align-bottom text-left rounded-20 shadow-md transform transition-all sm:align-middle w-max sm:max-w-6xl">
-                  <div className="flex flex-col gap-4  bg-overlay p-4 rounded-xl border border-transparent-color-gray-200 relative shadow-inner mt-24">
-                    <div className="text-white absolute top-2 right-2">
-                      <XIcon
-                        onClick={hide}
-                        className="w-6 h-6 cursor-pointer"
-                      ></XIcon>
-                    </div>
-                    <div className="text-center text-xl font-bold text-white">
-                      Your cart
-                    </div>
-                    {cart.length ? (
-                      <div className="flex flex-col items-center border border-transparent-color-gray-200 rounded-xl md:min-w-[500px] md:w-max py-2">
-                        <div className="flex justify-between gap-4 w-full">
-                          <h2 className="text-lg font-bold text-white opacity-[0.5] py-4 px-4">
-                            {cart
-                              .map((item: any) => item.quantity)
-                              .reduce((acc: any, red: any) => acc + red)}{" "}
-                            Item
-                            {cart
-                              .map((item: any) => item.quantity)
-                              .reduce((acc: any, red: any) => acc + red) > 1
-                              ? "s"
-                              : ""}
-                          </h2>{" "}
-                          <h2
-                            className="text-sm font-bold text-white py-4 px-4 cursor-pointer"
-                            onClick={() => {
-                              dispatch(removeAll());
-                            }}
-                          >
-                            Clear all
-                          </h2>
-                        </div>
-                        <div className="px-2 pb-4">
-                          <div className="gap-2 flex items-center justify-center w-full rounded-xl border border-overlay-border p-4">
-                            <div className="flex flex-col items-center justify-center gap-2 w-[210px]">
-                              <h2 className="text-white font-bold text-center">
-                                {cart[0]?.name}
-                              </h2>
-                              <img
-                                src="https://bafybeicvxsksypr4z6bkwey2dbtmzmm7dmu2eienae5zl25r6nb5u64kuu.ipfs.nftstorage.link/HVO_COVER.webp"
-                                alt=""
-                                className="w-[200px]"
-                              />
-                            </div>
-                            <img src="/icons/plus.svg" alt="" />
-                            <div className="flex flex-col items-center justify-center gap-2 w-[210px]">
-                              <h2 className="text-white font-bold text-center">
-                                {cart[1]?.name}
-                              </h2>
-                              <img
-                                src="https://bafybeicvxsksypr4z6bkwey2dbtmzmm7dmu2eienae5zl25r6nb5u64kuu.ipfs.nftstorage.link/HVO_COVER.webp"
-                                alt=""
-                                className="w-[200px]"
-                              />
-                            </div>
-                          </div>
-                        </div>
-                        <div className="text-md text-white font-bold w-full text-center">
-                          Chose currency
-                        </div>
-                        <div className="flex  gap-4 pb-4 w-full flex-wrap items-center justify-center shadow-inner">
-                          {tokensAllowed.map((item: any, index: any) => {
-                            return (
-                              <div
-                                className={clsx(
-                                  "w-24 flex items-center justify-center gap-1 rounded-xl cursor-pointer py-1 border border-white",
-
-                                  {
-                                    "bg-transparent-color-gray-200 border-none":
-                                      tokenSelected !== item.address,
-                                  },
-                                  {
-                                    "bg-overlay border-green-button shadow-[0_0px_10px] shadow-green-button":
-                                      tokenSelected === item.address,
-                                  },
-                                )}
-                                onClick={() => {
-                                  setTokenSelected(item.address);
-                                }}
-                              >
-                                <img
-                                  src={item.logo}
-                                  className="w-6 h-6"
-                                  alt=""
-                                />
-                                <h2 className="text-white text-sm font-bold">
-                                  {item.name}
-                                </h2>
-                              </div>
-                            );
-                          })}
-                        </div>
-                        <div className="flex gap-6 justify-between w-full text-md text-xl py-2 px-8 border-y border-transparent-color-gray-200 bg-transparent">
-                          <div className="flex gap-1 items-center">
-                            <h3 className="text-sm  text-white font-[700]">
-                              Total price:
-                            </h3>
-                          </div>
-                          <div className="flex flex-col gap items-end">
-                            {tokensAllowed.filter(
-                              (item: any) =>
-                                item.name === "MATIC" || item.name === "ETH",
-                            ).length > 0 && (
-                              <h3
-                                className="text-sm font-[700] text-white flex gap-1 items-center justify-center"
-                                style={{ fontSize: "14px" }}
-                              >
-                                {priceMatic} {isMatic ? "MATIC" : "ETH"}{" "}
-                                <img
-                                  src={
-                                    isMatic
-                                      ? "icons/polygon-matic-logo.png"
-                                      : "icons/eth.png"
-                                  }
-                                  className="w-3 h-3"
-                                  alt=""
-                                />
-                              </h3>
-                            )}
-                            <h3
-                              className="text-sm font-[700] text-white opacity-50"
-                              style={{ fontSize: "14px" }}
-                            >
-                              ($
-                              {parseInt(
-                                cart
-                                  ?.map((item: any, i: any) =>
-                                    (
-                                      parseInt(item.priceUSD) * item.quantity
-                                    ).toString(),
-                                  )
-                                  .reduce((item: any, acc: any) => {
-                                    return findSum(item, acc);
-                                  }),
-                              ) /
-                                10 ** 6}
-                              )
-                            </h3>
-                          </div>
-                        </div>
-
-                        {messageBuy !== "" ? (
-                          <div className="py-2 text-lg text-white font-bold text-center w-full">
-                            {messageBuy}
-                          </div>
-                        ) : (
-                          ""
-                        )}
-                        <div className="w-full flex items-center justify-center py-2">
-                          <div
-                            onClick={() => {
-                              buy();
-                            }}
-                            className="w-auto px-6 py-2 flex justify-center items-center rounded-xl hover:border-green-button hover:bg-overlay hover:text-green-button border border-transparent-color-gray-200 cursor-pointer bg-green-button font-bold text-overlay transition-all duration-500"
-                          >
-                            Checkout
-                          </div>
-                        </div>
-                        {providerName === "magic" && (
-                          <div
-                            className="text-[12px] text-green-button pt-4 font-bold flex items-center justify-center gap-2 cursor-pointer"
-                            onClick={() => {
-                              showWallet();
-                            }}
-                          >
-                            <img
-                              src="icons/wallet.png"
-                              className="w-8 pb-2"
-                              alt=""
-                            />{" "}
-                            Add funds to your wallet
-                          </div>
-                        )}
-                      </div>
-                    ) : (
-                      <div className="h-full flex flex-col items-center justify-center text-white font-bold gap-4 text-md text-center w-64 p-4 border border-transparent-color-gray-200 rounded-xl">
-                        <img
-                          src={"icons/logo.png"}
-                          className="w-20 h-20"
-                          alt=""
-                        />
-                        There aren't items in your cart.
-                      </div>
-                    )}
-                  </div>
-                </div>
-              </Transition.Child>
-            </div>
-          </Dialog>
-        </Transition.Root>
-      );
-    },
-    [],
-  );
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <div className="inline-block align-bottom text-left rounded-20 shadow-md transform transition-all sm:align-middle w-max sm:max-w-6xl">
+                {children}
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    );
+  }, []);
 
   return { Modal, isShow, hide, show };
 };
--- shared/components/Comics/Modals/index.tsx
+++ shared/components/Comics/Modals/index.tsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 
 import {
   editCartComics,
+  removeAll,
   removeAllComics,
   removeFromCartComics,
 } from "@redux/actions";
@@ -19,9 +20,10 @@ import {
   getTokensAllowedEth,
   switchChain,
 } from "@shared/web3";
-import { nFormatter } from "../../common/specialFields/SpecialFields";
+import { findSum, nFormatter } from "../../common/specialFields/SpecialFields";
 import { getDatabase, ref, set } from "firebase/database";
 import { toast } from "react-hot-toast";
+import useMagicLink from "@shared/hooks/useMagicLink";
 
 export const Modals = ({
   priceUSD,
@@ -89,7 +91,7 @@ export const Modals = ({
       };
 
       console.log(ids, amounts, token);
-      
+
       let price = "0";
       const ERC20 = getContractCustom("ERC20", token, providerEth);
       const addressesAllowed = getTokensAllowed();
@@ -180,99 +182,218 @@ export const Modals = ({
       {ModalAddress}
       <Modal
         noClose
-        cart={cartComics}
-        removeAll={removeAllComics}
-        messageBuy={messageBuy}
-        tokensAllowed={tokensAllowed}
         withoutX
-        tokenSelected={tokenSelected}
-        setTokenSelected={setTokenSelected}
         isShow={isShow}
         onClose={() => {
           setPreBuy(true);
         }}
-        buy={
-          preBuy
-            ? () => {
-                showAddress();
-                hide();
-              }
-            : buyComics
-        }
-        priceMatic={priceMatic}
-        isMatic={false}
-        itemsCart={cartComics.map((item, index) => {
-          return (
-            <div
-              className={
-                "py-2 flex items-center justify-between gap-8 text-white cursor-pointer w-full px-2 border border-transparent-color-gray-200 rounded-xl"
-              }
-              onClick={item.onClick}
-            >
-              {/* <div className="flex items-center justify-start gap-2 w-full">
-                <div className="rounded-xl flex flex-col text-gray-100 relative overflow-hidden border border-gray-500 h-20 w-20">
+      >
+        <CartComic
+          {...{
+            hide,
+            cart: cartComics,
+            tokensAllowed,
+            tokenSelected,
+            setTokenSelected,
+            priceMatic,
+            isMatic: true,
+            buy: preBuy
+              ? () => {
+                  showAddress();
+                  hide();
+                }
+              : buyComics,
+            messageBuy,
+          }}
+        />
+      </Modal>
+    </>
+  );
+};
+
+const CartComic = ({
+  hide,
+  cart,
+  tokensAllowed,
+  tokenSelected,
+  setTokenSelected,
+  priceMatic,
+  isMatic,
+  buy,
+  messageBuy,
+}) => {
+  const dispatch = useDispatch();
+  const { providerName } = useSelector((state: any) => state.layout.user);
+
+  const { showWallet } = useMagicLink();
+  return (
+    <div className="inline-block align-bottom text-left rounded-20 shadow-md transform transition-all sm:align-middle w-max sm:max-w-6xl">
+      <div className="flex flex-col gap-4  bg-overlay p-4 rounded-xl border border-transparent-color-gray-200 relative shadow-inner mt-24">
+        <div className="text-white absolute top-2 right-2">
+          <XIcon onClick={hide} className="w-6 h-6 cursor-pointer"></XIcon>
+        </div>
+        <div className="text-center text-xl font-bold text-white">
+          Your cart
+        </div>
+        {cart.length ? (
+          <div className="flex flex-col items-center border border-transparent-color-gray-200 rounded-xl md:min-w-[500px] md:w-max py-2">
+            <div className="flex justify-between gap-4 w-full">
+              <h2 className="text-lg font-bold text-white opacity-[0.5] py-4 px-4">
+                {cart
+                  .map((item: any) => item.quantity)
+                  .reduce((acc: any, red: any) => acc + red)}{" "}
+                Item
+                {cart
+                  .map((item: any) => item.quantity)
+                  .reduce((acc: any, red: any) => acc + red) > 1
+                  ? "s"
+                  : ""}
+              </h2>{" "}
+              <h2
+                className="text-sm font-bold text-white py-4 px-4 cursor-pointer"
+                onClick={() => {
+                  dispatch(removeAll());
+                }}
+              >
+                Clear all
+              </h2>
+            </div>
+            <div className="px-2 pb-4">
+              <div className="gap-2 flex items-center justify-center w-full rounded-xl border border-overlay-border p-4">
+                <div className="flex flex-col items-center justify-center gap-2 w-[210px]">
+                  <h2 className="text-white font-bold text-center">
+                    {cart[0]?.name}
+                  </h2>
                   <img
-                    src={comic[item.id - 1]?.comic_banner}
-                    className={`absolute bottom-0 top-0 left-[-40%] right-0 m-auto min-w-[175%]`}
+                    src="https://bafybeicvxsksypr4z6bkwey2dbtmzmm7dmu2eienae5zl25r6nb5u64kuu.ipfs.nftstorage.link/HVO_COVER.webp"
                     alt=""
+                    className="w-[200px]"
                   />
                 </div>
-                <div className="flex flex-col gap-1">
-                  <h3 className={"text-md font-[700] uppercase"}>
-                    {comic[item.id - 1].issues[item.idNFT]?.name}
-                  </h3>
-
-                  <div className="flex gap-2 items-end">
-                    <img src={"icons/logo.png"} className="w-8 h-8" alt="" />
-                    <img src="icons/POLYGON.svg" className="w-6 h-6" alt="" />
-                  </div>
+                <img src="/icons/plus.svg" alt="" />
+                <div className="flex flex-col items-center justify-center gap-2 w-[210px]">
+                  <h2 className="text-white font-bold text-center">
+                    {cart[1]?.name}
+                  </h2>
+                  <img
+                    src="https://bafybeicvxsksypr4z6bkwey2dbtmzmm7dmu2eienae5zl25r6nb5u64kuu.ipfs.nftstorage.link/HVO_COVER.webp"
+                    alt=""
+                    className="w-[200px]"
+                  />
                 </div>
-              </div> */}
-              {/* <div className="flex gap-2 shrink-0">
-                <div className="flex flex-col !shrink-0">
-                  <h3 className={"text-sm font-[700] whitespace-nowrap w-24"}>
-                    Price:
-                  </h3>
-                  <h3
-                    className={clsx(
-                      "text-sm font-[700] uppercase whitespace-nowrap w-24",
-                    )}
-                  >
-                    {nFormatter(parseInt(item.priceUSD) / 10 ** 6)} USD{" "}
-                  </h3>
-                </div>
-                {/* <input
-                  defaultValue={item.quantity}
-                  type="number"
-                  min={1}
-                  className="text-lg px-2 text-white w-12 bg-transparent rounded-xl border border-overlay-overlay"
-                  onChange={(e) => {
-                    dispatch(
-                      editCartComics({
-                        id: index,
-                        item: {
-                          ...item,
-                          quantity: e.target.value,
-                          priceUSD: priceUSD,
-                        },
-                      }),
-                    );
-                    getPriceMatic();
-                  }}
-                ></input> */}
-              {/* <div
-                  className="rounded-full p-1 w-8 h-8 border border-transparent-color-gray-200 hover:bg-red-primary text-white shrink-0 cursor-pointer"
-                  onClick={() => {
-                    dispatch(removeFromCartComics({ id: item.id }));
-                  }}
-                >
-                  <XIcon></XIcon>
-                </div> 
-              </div> */}
+              </div>
             </div>
-          );
-        })}
-      />
-    </>
+            <div className="text-md text-white font-bold w-full text-center">
+              Chose currency
+            </div>
+            <div className="flex  gap-4 pb-4 w-full flex-wrap items-center justify-center shadow-inner">
+              {tokensAllowed.map((item: any, index: any) => {
+                return (
+                  <div
+                    className={clsx(
+                      "w-24 flex items-center justify-center gap-1 rounded-xl cursor-pointer py-1 border border-white",
+
+                      {
+                        "bg-transparent-color-gray-200 border-none":
+                          tokenSelected !== item.address,
+                      },
+                      {
+                        "bg-overlay border-green-button shadow-[0_0px_10px] shadow-green-button":
+                          tokenSelected === item.address,
+                      },
+                    )}
+                    onClick={() => {
+                      setTokenSelected(item.address);
+                    }}
+                  >
+                    <img src={item.logo} className="w-6 h-6" alt="" />
+                    <h2 className="text-white text-sm font-bold">
+                      {item.name}
+                    </h2>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="flex gap-6 justify-between w-full text-md text-xl py-2 px-8 border-y border-transparent-color-gray-200 bg-transparent">
+              <div className="flex gap-1 items-center">
+                <h3 className="text-sm  text-white font-[700]">Total price:</h3>
+              </div>
+              <div className="flex flex-col gap items-end">
+                {tokensAllowed.filter(
+                  (item: any) => item.name === "MATIC" || item.name === "ETH",
+                ).length > 0 && (
+                  <h3
+                    className="text-sm font-[700] text-white flex gap-1 items-center justify-center"
+                    style={{ fontSize: "14px" }}
+                  >
+                    {priceMatic} {isMatic ? "MATIC" : "ETH"}{" "}
+                    <img
+                      src={
+                        isMatic
+                          ? "icons/polygon-matic-logo.png"
+                          : "icons/eth.png"
+                      }
+                      className="w-3 h-3"
+                      alt=""
+                    />
+                  </h3>
+                )}
+                <h3
+                  className="text-sm font-[700] text-white opacity-50"
+                  style={{ fontSize: "14px" }}
+                >
+                  ($
+                  {parseInt(
+                    cart
+                      ?.map((item: any, i: any) =>
+                        (parseInt(item.priceUSD) * item.quantity).toString(),
+                      )
+                      .reduce((item: any, acc: any) => {
+                        return findSum(item, acc);
+                      }),
+                  ) /
+                    10 ** 6}
+                  )
+                </h3>
+              </div>
+            </div>
+
+            {messageBuy !== "" ? (
+              <div className="py-2 text-lg text-white font-bold text-center w-full">
+                {messageBuy}
+              </div>
+            ) : (
+              ""
+            )}
+            <div className="w-full flex items-center justify-center py-2">
+              <div
+                onClick={() => {
+                  buy();
+                }}
+                className="w-auto px-6 py-2 flex justify-center items-center rounded-xl hover:border-green-button hover:bg-overlay hover:text-green-button border border-transparent-color-gray-200 cursor-pointer bg-green-button font-bold text-overlay transition-all duration-500"
+              >
+                Checkout
+              </div>
+            </div>
+            {providerName === "magic" && (
+              <div
+                className="text-[12px] text-green-button pt-4 font-bold flex items-center justify-center gap-2 cursor-pointer"
+                onClick={() => {
+                  showWallet();
+                }}
+              >
+                <img src="icons/wallet.png" className="w-8 pb-2" alt="" /> Add
+                funds to your wallet
+              </div>
+            )}
+          </div>
+        ) : (
+          <div className="h-full flex flex-col items-center justify-center text-white font-bold gap-4 text-md text-center w-64 p-4 border border-transparent-color-gray-200 rounded-xl">
+            <img src={"icons/logo.png"} className="w-20 h-20" alt="" />
+            There aren't items in your cart.
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
