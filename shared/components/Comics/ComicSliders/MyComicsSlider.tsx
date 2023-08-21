da8a883d567e7738013a9d42419bce88ceee312b
AN-Drew207 <andrescontrerasoviedo740@gmail.com>
2023-08-21 11:04:01 -0400

    adde swipper css

--- shared/components/Comics/ComicSliders/MyComicsSlider.tsx
+++ shared/components/Comics/ComicSliders/MyComicsSlider.tsx
@@ -7,6 +7,7 @@ import comicsByID from "@shared/comicsByNFTId.json";
 import Link from "next/link";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Zoom } from "swiper";
+import "swiper/css";
 
 function MyComics({ balance }) {
   const settings = {
@@ -17,7 +18,7 @@ function MyComics({ balance }) {
   };
 
   return (
-    <>
+    <div>
       <Flex mt={20} mb={10} justifyContent={"center"} id="my_comics">
         <Text
           fontSize={["30px", "50px", "65px", "4xl"]}
@@ -37,64 +38,62 @@ function MyComics({ balance }) {
         });
         return valid;
       }).length > 0 ? (
-        <div className="inline-block">
-          <Swiper
-            slidesPerView={2}
-            onSlideChange={() => console.log("slide change")}
-            onSwiper={(swiper) => console.log(swiper)}
-            zoom={true}
-            initialSlide={0}
-            modules={[Zoom]}
-            className="mySwiper w-full"
-            spaceBetween={10}
-            breakpoints={{
-              700: {
-                slidesPerView: 2,
-                centeredSlides: false,
-              },
-              100: {
-                slidesPerView: 1,
-                centeredSlides: true,
-              },
-            }}
-          >
-            {comicsByID
-              .filter((comic) => {
-                let valid = false;
-                balance.forEach((balance) => {
-                  if (balance.balance > 0 && balance.id === comic.id) {
-                    valid = true;
-                  }
-                });
-                return valid;
-              })
-              .map((item) => (
-                <SwiperSlide className="w-full sm:min-w-[450px] min-w-[300px]">
-                  <Flex key={Math.random().toString()} className="w-full">
-                    <Link href={`/comics/${item.nameLink}/${item.issue}`}>
-                      <Image
-                        className="images-width-comic-series cursor-pointer w-full"
-                        src={item.comic_banner}
-                        ml={2}
-                        mr={2}
-                        mt={2}
-                      ></Image>
-                    </Link>
-                    <Text
-                      className="human-vs-orges-text"
-                      position={"absolute"}
-                      bottom={[15, 5, 5, 5]}
-                      ml={5}
-                      color="#FFFFFF"
-                      fontSize={["12px", "12px", "16px", "18px"]}
-                    >
-                      {item.name}
-                    </Text>
-                  </Flex>
-                </SwiperSlide>
-              ))}
-          </Swiper>
-        </div>
+        <Swiper
+          slidesPerView={2}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+          zoom={true}
+          initialSlide={0}
+          modules={[Zoom]}
+          className="mySwiper w-full"
+          spaceBetween={10}
+          breakpoints={{
+            700: {
+              slidesPerView: 2,
+              centeredSlides: false,
+            },
+            100: {
+              slidesPerView: 1,
+              centeredSlides: true,
+            },
+          }}
+        >
+          {comicsByID
+            .filter((comic) => {
+              let valid = false;
+              balance.forEach((balance) => {
+                if (balance.balance > 0 && balance.id === comic.id) {
+                  valid = true;
+                }
+              });
+              return valid;
+            })
+            .map((item) => (
+              <SwiperSlide className="w-full sm:min-w-[450px] min-w-[300px]">
+                <Flex key={Math.random().toString()} className="w-full">
+                  <Link href={`/comics/${item.nameLink}/${item.issue}`}>
+                    <Image
+                      className="images-width-comic-series cursor-pointer w-full"
+                      src={item.comic_banner}
+                      ml={2}
+                      mr={2}
+                      mt={2}
+                    ></Image>
+                  </Link>
+                  <Text
+                    className="human-vs-orges-text"
+                    position={"absolute"}
+                    bottom={[15, 5, 5, 5]}
+                    ml={5}
+                    color="#FFFFFF"
+                    fontSize={["12px", "12px", "16px", "18px"]}
+                  >
+                    {item.name}
+                  </Text>
+                </Flex>
+              </SwiperSlide>
+            ))}
+        </Swiper>
       ) : (
         <div className="w-full py-24 flex items-center justify-center">
           <div className="h-full flex text-2xl flex-col items-center justify-center text-white font-bold gap-8 text-md text-center w-1/2 p-4">
@@ -103,7 +102,7 @@ function MyComics({ balance }) {
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 }
 
