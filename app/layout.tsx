5bf7f50175ac66c3cbb94187ed2abdfd22c0fa62
AN-Drew207 <andrescontrerasoviedo740@gmail.com>
2024-01-21 22:37:42 -0400

    fixed wallets

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,6 +58,24 @@ export default function RootLayout({ children }: { children: any }) {
     <html lang="en">
       <head>
         <title>Enders Gate Marketplace</title>
+        <link rel="icon" type="image/png" href="/icons/logo.png" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,900;1,300;1,400;1,600;1,900&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Amaranth&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
       </head>
 
       <body
